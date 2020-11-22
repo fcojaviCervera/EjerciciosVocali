d88adcde3416011e67ac7b387de5ebe30f30279c
fcojaviCervera <frankuko@hotmail.com>
2020-11-22 12:25:14 +0100

    Ejercicio 1 finalizado y ejercicio2 planteado.

---
+++ Ejercicio1/Periodo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+
+namespace Ejercicio1
+{
+    class Periodo 
+    {
+
+        public string Id { get; set; }
+        public string Ini { get; set; }
+        public string Fin { get; set; }
+
+        public Periodo(string _id, string _Ini, string _Fin)
+        {
+            Id = _id;
+            Ini = _Ini;
+            Fin = _Fin;
+        }
+
+        public bool IsValid()
+        {
+            //Expresion regular para validar la hora
+            Regex validHour = new Regex(@"^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$");
+
+            return
+                    (!string.IsNullOrEmpty(Id)) &&
+                    (!string.IsNullOrEmpty(Ini)) &&
+                    (!string.IsNullOrEmpty(Fin)) &&
+                    validHour.IsMatch(Ini) &&
+                    validHour.IsMatch(Fin);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Periodo && Equals((Periodo)obj);
+        }
+
+        public bool Equals(Periodo obj)
+        {
+            return (Ini.Equals(obj.Ini) && Fin.Equals(obj.Fin)) || Id.Equals(obj.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Ini.GetHashCode()+Fin.GetHashCode()+Id.GetHashCode();
+        }
+
+        /***
+         * Devuelve el periordo interseccion de los dos periodos, si lo hubiera. Si no hay intersección, devuelve nulo
+         */
+        public Periodo Intersect(Periodo obj)
+        {
+            TimeSpan timeSpanIni = TimeSpan.Parse(Ini);
+            TimeSpan timeSpanFin = TimeSpan.Parse(Fin);
+            TimeSpan objtimeSpanIni = TimeSpan.Parse(obj.Ini);
+            TimeSpan objtimeSpanFin = TimeSpan.Parse(obj.Fin);
+            TimeSpan minuto = TimeSpan.Parse("00:01");
+
+            if (timeSpanIni == objtimeSpanIni && timeSpanFin == objtimeSpanFin)
+            {
+                Console.WriteLine("\tPeriodos iguales. No intersectan. -> Id : {0} y {3} | Ini: {1} | Fin: {2}", Id, obj.Ini, obj.Fin,obj.Id);
+                return null;
+            }
+            else if (timeSpanIni<=objtimeSpanIni && timeSpanFin > objtimeSpanFin)
+            {
+                Console.WriteLine("\tPeriodo incluido dentro de otro. Separamos en 3 periodos -> Id : {0} | Ini: {1} | Fin: {2}", Id + obj.Id, obj.Ini, obj.Fin);
+                Periodo p = new Periodo(Id + obj.Id, obj.Ini, obj.Fin);
+
+                //Reutilizamos Id para indicar que se trata del mismo periodo
+                obj.Id = Id+"\'";
+                obj.Fin = Fin;
+                Fin = objtimeSpanIni.Subtract(minuto).ToString(@"hh\:mm");
+                obj.Ini = objtimeSpanFin.Add(minuto).ToString(@"hh\:mm");
+                
+
+                return p;
+            }
+            else if (timeSpanIni <= objtimeSpanIni && timeSpanFin > objtimeSpanIni)
+            {
+                Console.WriteLine("\tPeriodo interseccion-> Id : {0} | Ini: {1} | Fin: {2}", Id + obj.Id, obj.Ini, Fin);
+                Periodo p = new Periodo(Id + obj.Id, obj.Ini, Fin);
+                Fin = objtimeSpanIni.Subtract(minuto).ToString(@"hh\:mm");
+                obj.Ini = timeSpanFin.Add(minuto).ToString(@"hh\:mm");
+                return p;
+            }
+            else if (timeSpanIni <= objtimeSpanFin && timeSpanFin > objtimeSpanFin)
+            {
+                Console.WriteLine("\tPeriodo interseccion-> Id : {0} | Ini: {1} | Fin: {2}", obj.Id + Id, Ini, obj.Fin);
+                Periodo p = new Periodo(obj.Id + Id, Ini, obj.Fin);
+                Ini = objtimeSpanFin.Add(minuto).ToString(@"hh\:mm");
+                obj.Fin = timeSpanIni.Subtract(minuto).ToString(@"hh\:mm");
+                return p;
+            }            
+
+            return null;
+        }
+
+
+    }
+}
---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            /***    
+             * 
+             *  Dado un conjunto de N periodos, calcular la suma de estos teniendo en cuenta lo siguiente:
+                 Un periodo está identificado por un id único, y tiene una hora de Inicio (mayor o
+                igual que 00:00) y una hora de fin (menor o igual que 23:59).
+
+                Asumiremos que la hora de Inicio siempre será menor que la hora de fin.
+
+                La suma de 2 periodos se calcula de la siguiente forma: los periodos solapados
+                se combinan en uno nuevo, de forma que el resultado final sea otro conjunto de
+                periodos que no se solapen.
+
+                El conjunto de períodos se especificará mediante un fichero CSV donde cada
+                período vendrá representado por la terna id,Ini,fin con formato:
+
+                o Id: string. Ej: A
+                o Ini: string donde se indica una hora en formato mm:ss. Ej: 08:15
+                o Fin: string donde se indica una hora en formato mm:ss. Ej: 08:15
+
+                En el ejemplo siguiente la suma de Periodo 1 y Periodo 2 da como resultado tres
+                periodos:
+                - [id=A, Ini=03:00, fin=05:59]
+                - [id=AB, Ini=06:00, fin=08:00]
+                - [id=B, Ini=08:01, fin=09:00]
+             * 
+             */
+
+            //Consideraciones a tener en cuenta
+            /**
+             * Tener en cuenta valores nulos o vacios, si hay valor vacio se descarta el conjunto
+             * MInimo tiene que haber un conjunto, y el resultado del programa será el mismo conjunto
+             * Tener en cuenta repetidos, si hay un repetido se elimina el resultado
+             * Si ya existe un periodo con el mismo id, se descarta              
+             * Si no se indica alguna de las horas, o no cumplen el formato mm:ss se descarta 
+             
+             
+             */
+            Console.WriteLine("Ejercicio 1");
+
+            string fichero = ConfigurationManager.AppSettings["RutaEjecucion"] + ConfigurationManager.AppSettings["NombreFichero"];
+
+            Console.WriteLine("Por favor, asegurese de que el fichero CSV se encuentra en la carpeta: " + fichero);
+            Console.ReadKey();
+
+            HashSet<Periodo> periodos = LeerFichero(fichero);
+
+            HashSet<Periodo> periodosProcesados = ProcesarEntrada(periodos);
+
+            periodos.UnionWith(periodosProcesados);
+
+            
+            Console.WriteLine("Periodos tras procesar el fichero");
+
+            foreach (var item in periodos)
+            {
+                Console.WriteLine("\tPeriodo -> Id : {0} | Ini: {1} | Fin: {2}", item.Id, item.Ini, item.Fin);
+            }
+
+
+            Console.WriteLine("Fin de procesamiento de fichero");
+        }
+
+        private static HashSet<Periodo> ProcesarEntrada(HashSet<Periodo> periodos)
+        {
+            HashSet<Periodo> salida = new HashSet<Periodo>();
+            List<Periodo> copia = new List<Periodo>();
+            copia = periodos.ToList();
+
+
+            foreach (var item in periodos)
+            {
+                //Se elimina el propio elemento para evitar iterar sobre el mismo                
+                //copia.Remove(item);
+
+                foreach (var item2 in copia)
+                {
+                    Periodo interseccion = item.Intersect(item2);
+                    if (interseccion != null)
+                    {
+                        salida.Add(interseccion);
+                    }
+                }
+
+            }
+            return salida;
+
+        }
+
+        public static HashSet<Periodo> LeerFichero(string rutaCompleta)
+        {
+            HashSet<Periodo> periodos = new HashSet<Periodo>();
+            try
+            {
+                var reader = new StreamReader(File.OpenRead(rutaCompleta));
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    Periodo periodo = new Periodo(values[0].Trim(), values[1].Trim(), values[2].Trim());
+
+                    if (periodo.IsValid())
+                    {
+                        if (periodos.Add(periodo))
+                        {
+                            Console.WriteLine("\tPeriodo añadido al conjunto-> Id : {0} | Ini: {1} | Fin: {2}", periodo.Id, periodo.Ini, periodo.Fin);
+                        }
+                        else
+                        {
+                            Console.WriteLine("\tPeriodo ya existente en el conjunto-> Id : {0} | Ini: {1} | Fin: {2}", periodo.Id, periodo.Ini, periodo.Fin);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\tPeriodo incorrecto, no se añade al conjunto-> Id : {0} | Ini: {1} | Fin: {2}", periodo.Id, periodo.Ini, periodo.Fin);
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("No se ha encontrado el fichero especificado, por favor, revise si el fichero existe y la ruta está bien indicado en el fichero de configuracion");
+            }
+            return periodos;
+        }
+    }
+}
